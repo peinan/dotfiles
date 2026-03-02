9e957232fad28cae8d2ff1bb87bb89da27835a54
Peinan Zhang <peinan7@gmail.com>
2026-03-02 13:56:34 +0900

    feat(zsh): expand multi-dot cd input via zle (fix #38)
    - Remove .., ..2, ..3 cd aliases from .alias
    - Add zeno snippets for cd .., cd ../.., and cd ../../..
    - Bind '.' to a zle widget that expands repeated dots into ../.. style paths, with an exclusion for `go` commands

--- src/.zsh/configs/keybindings.zsh
+++ src/.zsh/configs/keybindings.zsh
@@ -2,3 +2,27 @@
 autoload -Uz edit-command-line
 zle -N edit-command-line
 bindkey "^O" edit-command-line
+
+# Expand multiple dots (e.g. ... -> ../.. , .... -> ../../..)
+# https://qiita.com/momo-lab/items/523fc83fbfa39fa5fd60
+function replace_multiple_dots() {
+  local dots=$LBUFFER[-2,-1]
+  if [[ $dots == ".." ]]; then
+    LBUFFER=$LBUFFER[1,-3]'../.'
+  fi
+  zle self-insert
+}
+zle -N replace_multiple_dots
+bindkey "." replace_multiple_dots
+
+# https://www.m3tech.blog/entry/dotfiles-bonsai
+replace_multiple_dots_exclude_go() {
+    if [[ "$LBUFFER" =~ '^go ' ]]; then
+        zle self-insert
+    else
+        zle .replace_multiple_dots
+    fi
+}
+zle -N .replace_multiple_dots replace_multiple_dots
+zle -N replace_multiple_dots replace_multiple_dots_exclude_go
+
